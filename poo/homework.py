646ba397260ae9c0a9028aeb2f64047b6b3a4385
daraeulalia31 <DaraEulalia31@gmail.com>
2024-10-02 19:31:19 -0500

    tarea

--- poo/homework.py
+++ poo/homework.py
@@ -26,3 +26,26 @@ print(carlos.retirar())
 #crear una clase agencia 
 # con sus atributos nombre y apellidos del pasajero dni numero de asiento fecha de viaje.
 # sus metodos seran ingresar origen, ingresar destino, cancelar viaje, ver estado de pasaje.
+
+class agencia:
+    #atributos
+    def _init_(self,nombre,apellidos,dni,numero_asiento,fecha_viaje):
+        self.nombre=nombre
+        self.apellidos=apellidos
+        self.dni=dni
+        self.numero_asiento=numero_asiento
+        self.fecha_viaje=fecha_viaje
+    #metodos
+    def ingresar_origen(self,lugar):
+        print("estoy ingresando de donde viajo:",lugar)
+    def ingresar_destino(self):
+        print("estoy ingresando de donde voy")
+    def cancelar_viaje(self):
+        print("estoy cancelando mi viaje ")
+    def ver_estado_pasaje(self):
+        print("estoy viendo el estado de mi pasaje")
+eulalia=agencia("eulalia","tito olivares",7446489,122,"2024/12/22")
+print(eulalia.ingresar_destino())
+eulalia.ingresar_origen("puquio")
+
+
